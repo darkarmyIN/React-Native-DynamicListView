ed92a7723e9f066c6a9a235884328cffd545157a
Shubham Sorte <luk4shubhs@gmail.com>
2016-08-16 21:05:30 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -7,29 +7,41 @@ import {
   Text,
   TouchableHighlight
 } from 'react-native';
-var superheroArray = [];
+var productArray = [];
 class Home extends Component {
   constructor(props) {
     super(props);
     var dataSource = new ListView.DataSource({rowHasChanged:(r1,r2) => r1.guid != r2.guid});
     this.state = {
-      dataSource: dataSource.cloneWithRows(superheroArray)
+      dataSource: dataSource.cloneWithRows(productArray),
+      isLoading:true
     }
   }
 
   componentDidMount() {
-    var url = "";
+    this.getTheData(function(json){
+    productArray = json;
+    this.setState = ({
+      datasource:this.state.dataSource.cloneWithRows(productArray),
+      isLoading:false
+    })
+  }.bind(this));
+    
+  }
+
+  getTheData(callback) {
+    var url = "https://raw.githubusercontent.com/darkarmyIN/React-Native-DynamicListView/master/appledata.json";
     fetch(url)
     .then(response => response.json())
     .then(json => callback(json))
     .catch(error => console.log(error));
   }
-
+  
   renderRow(rowData, sectionID, rowID) {
  	return (
     	<TouchableHighlight underlayColor='#dddddd' style={{height:44}}>
         <View>
-        <Text style={{fontSize: 20, color: '#000000'}} numberOfLines={1}>{rowData}</Text>
+        <Text style={{fontSize: 20, color: '#000000'}} numberOfLines={1}>{rowData.display_string}</Text>
         <View style={{height: 1, backgroundColor: '#dddddd'}}/>
         </View>
     	</TouchableHighlight>
@@ -37,10 +49,12 @@ class Home extends Component {
 }
 
   render() {
+    var currentView = (this.state.isLoading)?<View/>:<ListView dataSource={this.state.dataSource} renderRow={this.renderRow.bind(this)} enableEmptySections={true}/>
 
     return(
-      <ListView dataSource={this.state.dataSource} renderRow={this.renderRow.bind(this)} enableEmptySections={true}>
-      </ListView>
+      <View>
+      {currentView}
+      </View>
     );
   }
 
